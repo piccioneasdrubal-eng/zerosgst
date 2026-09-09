ec58f12ce94c350cc2a43bb1ba96f0ff3b0fa484
piccioneasdrubal-eng <piccioneasdrubal@gmail.com>
2026-09-09 02:07:26 +0200

    Update game.js

--- game.js
+++ game.js
@@ -10,8 +10,8 @@ const path = require('path');
 const CONFIG = {
   PORT: Number(process.env.PORT) || 3000,
   WORLD: {
-    WIDTH: 10000,
-    HEIGHT: 10000,
+    WIDTH: 100%,
+    HEIGHT: 100%,
     PELLET_COUNT: 4200,
     MAX_PELLETS: 4700,
     PELLET_MASS: 10,
